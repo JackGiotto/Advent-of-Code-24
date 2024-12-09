30e210757a7ef836073df85c16250d9af642bda9
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-12-09 12:55:18 +0100

    day 9 challenge 2

--- day_9/day_9.cpp
+++ day_9/day_9.cpp
@@ -50,6 +50,34 @@ void useFreeSpace(std::vector<int> &ar) {
 	}
 }
 
+void useFreeSpaceFullBlocks(std::vector<int>& ar) {
+	int started_block = ar.size()-1;
+	for (size_t i = ar.size() - 1; i > 0; i--) {
+		if (!(i == ar.size()-1) && ar[i] != ar[i+1]) {
+			if (started_block != i) {
+				size_t j;
+				for (j = 0; j < i; j++) {
+					if (ar[j] == -1) {
+						size_t k;
+						for (k = j; ar[k] == -1; k++) {
+						}
+						if (k-j >= started_block-i) {
+							size_t h;
+							for (k=j, h = started_block; h>i; k++, h--) {
+								std::swap(ar[k], ar[h]);
+							}
+							break;
+						} else {
+							j=k;
+						}
+					}
+				}
+				started_block = i;
+			}
+		}
+	}
+}
+
 void deleteEmpty(std::vector<int> &ar) {
 	int i = 0;
 	while (ar[i] != -1) {
@@ -62,19 +90,26 @@ void deleteEmpty(std::vector<int> &ar) {
 long int calcCheckSum(const std::vector<int>& ar) {
 	long int sum =0;
 	for (size_t i = 1; i < ar.size(); i++) {
-		sum += ar[i] * i;
+		if (ar[i] != -1)
+			sum += ar[i] * i;
 	}
 	return sum;
 }
 
 int main(int argc, char* argv[]) {
 	std::vector<int> ar = readFile("input.txt");
-	//printVector(ar);
-	useFreeSpace(ar);
-	deleteEmpty(ar);
-	//printVector(ar);
+	std::vector<int> tmp_ar = ar;
 
-	long int checksum = calcCheckSum(ar);
+	// Challenge 1
+	useFreeSpace(tmp_ar);
+	deleteEmpty(tmp_ar);
+	long int checksum = calcCheckSum(tmp_ar);
 	std::cout << "Checksum: " << checksum << std::endl;
+
+	// Challenge 2
+	useFreeSpaceFullBlocks(ar);
+	checksum = calcCheckSum(ar);
+	std::cout << "Checksum with full blocks: " << checksum << std::endl;
+
 	return 0;
 }
